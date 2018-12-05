ad5201e0997295527e942a890581839f0da26cde
qiang.lv <xzsdeveloper08@gmail.com>
2018-12-05 19:10:39 +0800

    fix #2

--- trackstar/protected/models/Issue.php
+++ trackstar/protected/models/Issue.php
@@ -154,5 +154,23 @@ class Issue extends CActiveRecord
 		return new CActiveDataProvider(get_class($this), array(
 			'criteria'=>$criteria,
 		));
+		
+	}
+	
+	/**
+	 * @return string the status text display for the current issue
+	 */
+	public function getStatusText()
+	{
+	    $statusOptions=$this->statusOptions;
+	    return isset($statusOptions[$this->status_id]) ? $statusOptions[$this->status_id] : "unknown status ({$this->status_id})";
+	}
+	/**
+	 * @return string the type text display for the current issue
+	 */
+	public function getTypeText()
+	{
+	    $typeOptions=$this->typeOptions;
+	    return isset($typeOptions[$this->type_id]) ? $typeOptions[$this->type_id] : "unknown type ({$this->type_id})";
 	}
 }
