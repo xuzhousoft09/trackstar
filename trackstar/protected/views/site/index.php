af4e37ef8bf1b34a2504357e46cacb44806617b3
qiang.lv <xzsdeveloper@gmail.com>
2018-12-10 15:42:53 +0800

    change the top page to display the last login time.

--- trackstar/protected/views/site/index.php
+++ trackstar/protected/views/site/index.php
@@ -2,6 +2,14 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
+<?php if(!Yii::app()->user->isGuest):?>
+<p>
+You last logged in on <?php echo date( 'l, F d, Y, g:i a',
+Yii::app()->user->lastLoginTime ); ?>.
+</p>
+<?php endif;?>
+
+<!--  
 <p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
@@ -13,4 +21,4 @@
 <p>For more details on how to further develop this application, please read
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+should you have any questions.</p>-->
