9b5cd537779d584d361754a114b13f0478e2b135
qiang.lv <xzsdeveloper@gmail.com>
2018-12-15 15:38:53 +0800

    create the user module

--- trackstar/protected/config/main.php
+++ trackstar/protected/config/main.php
@@ -41,14 +41,14 @@ return array(
 		),
 		// uncomment the following to enable URLs in path-format
 		
-		'urlManager'=>array(
+	/* 	'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
-		),
+		), */
 		
 		
 			'authManager'=>array(
