18bc1d362961490851792448bc77e2756f37729c
qiang.lv <xzsdeveloper@gmail.com>
2018-12-15 14:39:14 +0800

    change the url to user readable

--- trackstar/protected/config/main.php
+++ trackstar/protected/config/main.php
@@ -40,7 +40,7 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -49,7 +49,7 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		
 		
 			'authManager'=>array(
 					'class'=>'CDbAuthManager',
