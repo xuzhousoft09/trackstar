686fb65fbbbd1da60bfb194ca62f2556df8b37d4
qiang.lv <xzsdeveloper@gmail.com>
2018-12-16 17:44:52 +0800

    only the admin user can access the admin/sysmessage/admin action.

--- trackstar/protected/modules/admin/controllers/SysMessageController.php
+++ trackstar/protected/modules/admin/controllers/SysMessageController.php
@@ -28,19 +28,28 @@ class SysMessageController extends Controller
 
 	public function accessRules()
 		{
+
+			
 			return array(
-				array('allow',  // allow only users in the 'admin' role access to our actions
-					'actions'=>array('index','view', 'create', 'update', 'admin', 'delete'),
-					'roles'=>array('admin'),
-				),
-					 array('allow',  // allow only users in the 'admin' role access to our actions
+					array('allow',
 							'actions'=>array('index','view'),
-							'roles'=>array('@'),
-					), 
-			 /* 	array('deny',  // deny all users
-					'users'=>array('*'),
-				),  */
+							'users'=>array('@'),
+					),
+						
+					array('allow', // allow authenticated user to perform 'create' and 'update' actions
+							'actions'=>array('create','update'),
+							'users'=>array('@'),
+					),
+					array('allow', // allow admin user to perform 'admin' and 'delete' actions
+							'actions'=>array('admin','delete'),
+							'users'=>array('admin'),
+					),
+					array('deny',  // deny all users
+							'users'=>array('*'),
+					),
 			);
+			
+			
 		} 
 
 	/**
