08be79a521259a119b4afbb7e2b9ff6b12bf9139
qiang.lv <xzsdeveloper@gmail.com>
2018-12-22 17:34:52 +0800

    add the upload menu item

--- trackstar/protected/modules/admin/views/layouts/main.php
+++ trackstar/protected/modules/admin/views/layouts/main.php
@@ -39,7 +39,9 @@
 					
 					array('label'=>'System Message', 'url'=>array('/admin/sysMessage/index')),
 					/* array('label'=>'Users', 'url'=>array('/user/index'),'visible'=>Yii::app()->user->checkAccess("admin")), */
+					array('label'=>'Upload', 'url'=>array('/upload/index')),
 					array('label'=>'Users', 'url'=>array('/user/index')),
+					
 					array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 					),
